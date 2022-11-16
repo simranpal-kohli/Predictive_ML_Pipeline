afa6571612e5dadbf79542f5e338fbddc4e7eb6f
Simranpal Kohli <simranpalkh20@gmail.com>
2022-11-16 23:51:09 +0000

    Update churn_model_pdm.sh

--- churn_model_pdm.sh
+++ churn_model_pdm.sh
@@ -1,9 +1,1 @@
-#!bash
 
-# jupyter nbconvert --to notebook --execute /home/jovyan/Simranpal/Commscoe/lookup_mismatch/Lookup_Mismatch_Commscoe.ipynb
-
-# jupyter nbconvert --execute --to notebook --inplace /home/jovyan/Simranpal/Commscoe/lookup_mismatch/Lookup_Mismatch_Commscoe.ipynb
-# --output /home/jovyan/Simranpal/Commscoe/lookup_mismatch/Lookup_Mismatch_Commscoe.ipynb
-
-jupyter nbconvert --execute --to notebook --inplace /home/jovyan/Simranpal/All_Queue/Churn_Model/main_pm.ipynb
---output /home/jovyan/Simranpal/All_Queue/Churn_Model/main_pm.ipynb
